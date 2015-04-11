cdcbd5a0f240378352ba8ad12811dc2a983f09f8
ian jamieson <cogocreative@gmail.com>
2015-04-11 19:06:03 +0200

    index resource

---
+++ app/routes/index.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    // Tell ember what Model listings will use
+    model: function() {
+        return this.store.find('listing');
+    }
+});
