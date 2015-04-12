135fa27a46b9c60290d06b0a19f7e542105acfe6
ian jamieson <cogocreative@gmail.com>
2015-04-12 18:40:36 +0200

    delete working

--- app/controllers/manage/listings.js
+++ app/controllers/manage/listings.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
     actions: {
         removeListing: function(listing) {
-            console.log('removeListing', listing);
-            listing.destroyRecord();
+            var controller = this;
+            /// Delete and go to manage/listings
+            listing.destroyRecord().then(function() {
+                controller.transitionToRoute('manage.listings');
+            });
         }
     }
 });
